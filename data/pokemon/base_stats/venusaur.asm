a71e6692ccdedd37108b01664a6469cd2f901178
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-02-11 16:44:46 -0600

    [UPD] - Updated some Pokémon
    Updated the TM/HM learn set of BULBASAUR, IVYSAUR, VENUSAUR, CHARIZARD

--- data/pokemon/base_stats/venusaur.asm
+++ data/pokemon/base_stats/venusaur.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm SWORDS_DANCE, TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
 	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLARBEAM,    MIMIC,        \
-	     DOUBLE_TEAM,  REFLECT,      BIDE,         REST,         SUBSTITUTE,   \
-	     CUT
+	     DOUBLE_TEAM,  REFLECT,      BIDE,         SKULL_BASH,   REST,         \
+			 SUBSTITUTE,   CUT,          STRENGTH,     FLASH
 	; end
 
 	db 0 ; padding
