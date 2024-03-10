e3bad1f818799cc51fc1ab14a728d45a098579de
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 14:46:15 -0600

    [FIX] Updated raticate.asm
    Improved legibility on file

--- data/pokemon/base_stats/raticate.asm
+++ data/pokemon/base_stats/raticate.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,   \
 	     WATER_GUN,    ICE_BEAM,     BLIZZARD,     HYPER_BEAM,   RAGE,         \
-			 THUNDERBOLT,  THUNDER,      DIG,          MIMIC,        DOUBLE_TEAM,  \
-			 BIDE,         METRONOME,    SWIFT,        SKULL_BASH,   REST,         \
+	     THUNDERBOLT,  THUNDER,      DIG,          MIMIC,        DOUBLE_TEAM,  \
+	     BIDE,         METRONOME,    SWIFT,        SKULL_BASH,   REST,         \
 	     SUBSTITUTE,   CUT
 	; end
 
