f33e432990623f33f73cb496ac53a197c2c75ec1
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 13:16:53 -0600

    [FIX] Updated ivysaur.asm
    Improved legibility on file

--- data/pokemon/base_stats/ivysaur.asm
+++ data/pokemon/base_stats/ivysaur.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm SWORDS_DANCE, TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
 	     RAGE,         MEGA_DRAIN,   SOLARBEAM,    MIMIC,        DOUBLE_TEAM,  \
-			 REFLECT,      BIDE,         SKULL_BASH,   REST,         SUBSTITUTE,   \
-			 CUT,          FLASH
+	     REFLECT,      BIDE,         SKULL_BASH,   REST,         SUBSTITUTE,   \
+	     CUT,          FLASH
 	; end
 
 	db 0 ; padding
