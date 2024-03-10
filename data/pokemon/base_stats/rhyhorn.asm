2e0cdae2ee3f103d2c9cba7b6add4497b1d3b8c7
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 14:47:33 -0600

    [FIX] Updated rhyhorn.asm
    Improved legibility on file

--- data/pokemon/base_stats/rhyhorn.asm
+++ data/pokemon/base_stats/rhyhorn.asm
@@ -16,9 +16,9 @@
 	; tm/hm learnset
 	tmhm SWORDS_DANCE, TOXIC,        HORN_DRILL,   BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  RAGE,         THUNDERBOLT,  THUNDER,      EARTHQUAKE,   \
-			 FISSURE,      DIG,          MIMIC,        DOUBLE_TEAM,  BIDE,         \
-			 FIRE_BLAST,   SKULL_BASH,   REST,         ROCK_SLIDE,   SUBSTITUTE,   \
-			 STRENGTH
+	     FISSURE,      DIG,          MIMIC,        DOUBLE_TEAM,  BIDE,         \
+	     FIRE_BLAST,   SKULL_BASH,   REST,         ROCK_SLIDE,   SUBSTITUTE,   \
+	     STRENGTH
 	; end
 
 	db 0 ; padding
