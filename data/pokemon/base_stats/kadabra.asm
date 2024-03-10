a1a887b9c8bd7d69d52f33098c9ac90a39341efc
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 13:25:05 -0600

    [FIX] Updated kadabra.asm
    Improved legibility on file

--- data/pokemon/base_stats/kadabra.asm
+++ data/pokemon/base_stats/kadabra.asm
@@ -16,7 +16,7 @@
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  BLIZZARD,     SUBMISSION,   COUNTER,      SEISMIC_TOSS, \
-			 RAGE,         THUNDER,      PSYCHIC_M,    TELEPORT,     MIMIC,        \
+	     RAGE,         THUNDER,      PSYCHIC_M,    TELEPORT,     MIMIC,        \
 	     DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    FIRE_BLAST,   \
 	     SWIFT,        SKULL_BASH,   DREAM_EATER   REST,         THUNDER_WAVE, \
 	     PSYWAVE,      TRI_ATTACK,   SUBSTITUTE,   FLASH
