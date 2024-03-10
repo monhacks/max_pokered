9474848accb09236ca79fda1b36229545b4e4923
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 13:51:50 -0600

    [FIX] Updated poliwag.asm
    Improved legibility on file

--- data/pokemon/base_stats/poliwag.asm
+++ data/pokemon/base_stats/poliwag.asm
@@ -17,7 +17,7 @@
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,   \
 	     WATER_GUN,    ICE_BEAM,     BLIZZARD,     RAGE,         PSYCHIC_M,    \
 	     MIMIC,        DOUBLE_TEAM,  BIDE,         SWIFT,        SKULL_BASH,   \
-			 REST,         PSYWAVE,      SUBSTITUTE,   SURF,         FLASH
+	     REST,         PSYWAVE,      SUBSTITUTE,   SURF,         FLASH
 	; end
 
 	db 0 ; padding
