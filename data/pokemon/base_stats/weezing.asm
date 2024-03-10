2afba676900cfd84e065ff05a398fdfeb019390b
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 14:57:39 -0600

    [FIX] Updated weezing.asm
    Improved legibility on file

--- data/pokemon/base_stats/weezing.asm
+++ data/pokemon/base_stats/weezing.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  ICE_BEAM,     BLIZZARD,     \
 	     HYPER_BEAM,   RAGE,         THUNDERBOLT,  THUNDER,      MIMIC,        \
-			 DOUBLE_TEAM,  REFLECT,      BIDE,         SELFDESTRUCT, FIRE_BLAST,   \
-			 SKULL_BASH,   REST,         EXPLOSION,    SUBSTITUTE,   FLASH
+	     DOUBLE_TEAM,  REFLECT,      BIDE,         SELFDESTRUCT, FIRE_BLAST,   \
+	     SKULL_BASH,   REST,         EXPLOSION,    SUBSTITUTE,   FLASH
 	; end
 
 	db 0 ; padding
