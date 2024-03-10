86c1e26731f415b3f9a1cc3a9664fe2b0d1b8186
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 14:51:30 -0600

    [FIX] Updated slowpoke.asm
    Improved legibility on file

--- data/pokemon/base_stats/slowpoke.asm
+++ data/pokemon/base_stats/slowpoke.asm
@@ -18,9 +18,9 @@
 	     WATER_GUN,    ICE_BEAM,     BLIZZARD,     PAY_DAY,      RAGE,         \
 	     EARTHQUAKE,   FISSURE,      DIG,          PSYCHIC_M,    TELEPORT,     \
 	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
-			 FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, \
-			 PSYWAVE,      TRI_ATTACK,   SUBSTITUTE,   SURF,         STRENGTH,     \
-			 FLASH
+	     FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, \
+	     PSYWAVE,      TRI_ATTACK,   SUBSTITUTE,   SURF,         STRENGTH,     \
+	     FLASH
 	; end
 
 	db 0 ; padding
