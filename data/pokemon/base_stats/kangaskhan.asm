11d6125cb0025802dcc3aaa03376349b68a0645c
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 13:25:32 -0600

    [FIX] Updated kangaskhan.asm
    Improved legibility on file

--- data/pokemon/base_stats/kangaskhan.asm
+++ data/pokemon/base_stats/kangaskhan.asm
@@ -17,9 +17,9 @@
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     BLIZZARD,     \
 	     HYPER_BEAM,   PAY_DAY,      SUBMISSION,   COUNTER,      SEISMIC_TOSS, \
-			 RAGE,         THUNDERBOLT,  THUNDER,      EARTHQUAKE,   FISSURE,      \
-			 MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
-			 FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, \
+	     RAGE,         THUNDERBOLT,  THUNDER,      EARTHQUAKE,   FISSURE,      \
+	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
+	     FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, \
 	     ROCK_SLIDE,   SUBSTITUTE,   SURF,         STRENGTH
 	; end
 
