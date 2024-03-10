9510fc536b30781564543f6b668dc81c3e594381
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 13:35:22 -0600

    ]Updated mrmime.asm

--- data/pokemon/base_stats/mrmime.asm
+++ data/pokemon/base_stats/mrmime.asm
@@ -16,10 +16,10 @@
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  HYPER_BEAM,   PAY_DAY,      SUBMISSION,   COUNTER,      \
-			 SEISMIC_TOSS, RAGE,         SOLARBEAM,    THUNDERBOLT,  THUNDER,      \
-			 PSYCHIC_M,    TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      \
-			 BIDE,         METRONOME,    SWIFT,        SKULL_BASH,   DREAM_EATER,  \
-			 REST,         THUNDER_WAVE, PSYWAVE,      SUBSTITUTE,   FLASH
+	     SEISMIC_TOSS, RAGE,         SOLARBEAM,    THUNDERBOLT,  THUNDER,      \
+	     PSYCHIC_M,    TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      \
+	     BIDE,         METRONOME,    SWIFT,        SKULL_BASH,   DREAM_EATER,  \
+	     REST,         THUNDER_WAVE, PSYWAVE,      SUBSTITUTE,   FLASH
 	; end
 
 	db 0 ; padding
