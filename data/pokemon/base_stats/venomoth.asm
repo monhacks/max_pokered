a8e6fc0cb5eb206ad39c48c200d8629d764f6534
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 14:55:45 -0600

    [FIX] Updated venomoth.asm
    Improved legibility on file

--- data/pokemon/base_stats/venomoth.asm
+++ data/pokemon/base_stats/venomoth.asm
@@ -18,7 +18,7 @@
 	     HYPER_BEAM,   RAGE,         MEGA_DRAIN,   SOLARBEAM,    PSYCHIC_M,    \
 	     TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
 	     METRONOME,    SWIFT,        REST,         PSYWAVE,      SUBSTITUTE,   \
-			 FLASH
+	     FLASH
 	; end
 
 	db 0 ; padding
