465124504d250edd0f17dd060b8038c275bf0f64
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-03-10 12:32:46 -0600

    [FIX] Updated dodrio.asm
    Improved legibility on file

--- data/pokemon/base_stats/dodrio.asm
+++ data/pokemon/base_stats/dodrio.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
 	     DOUBLE_EDGE,  HYPER_BEAM,   RAGE,         MIMIC,        DOUBLE_TEAM,  \
-			 REFLECT,      BIDE,         SWIFT,        SKULL_BASH,   SKY_ATTACK,   \
-			 REST,         TRI_ATTACK,   SUBSTITUTE,   FLY
+	     REFLECT,      BIDE,         SWIFT,        SKULL_BASH,   SKY_ATTACK,   \
+	     REST,         TRI_ATTACK,   SUBSTITUTE,   FLY
 	; end
 
 	db 0 ; padding
